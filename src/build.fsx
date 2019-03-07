2f1c7e0895a5abee7547e5bc2b11e33c766c7ec6
Дмитрий Ушенко <dmitry.ushenko@redkassa.ru>
2019-03-07 14:07:04 +0300

    Build with build.fsx

--- src/build.fsx
+++ src/build.fsx
@@ -1,12 +1,67 @@
 #r "paket:
+source https://api.nuget.org/v3/index.json
+nuget FSharp.Core
+nuget Fake.IO.FileSystem
+nuget Fake.DotNet.Cli
+nuget Fake.Core.Globbing
 nuget Fake.Core.Target //"
 
+open Fake.IO
+open Fake.Core.Globbing.Operators
 open Fake.Core
+open Fake.DotNet
+open Fake.IO.FileSystemOperators
 
-// Default target
-Target.create "Default" (fun _ ->
-  Trace.trace "Hello World from FAKE"
+let configuration           = Environment.environVarOrDefault "configuration"            "Debug"
+let debugsymbols            = Environment.environVarOrDefault "debugsymbols"             "True"
+let optimize                = Environment.environVarOrDefault "optimize"                 "False"
+let targetframeworkversion  = Environment.environVarOrDefault "targetframeworkversion"   "netcoreapp2.2"
+
+
+Target.create "Initialize" (fun _ ->
+
+    Trace.trace <| sprintf "working dir:            %s" __SOURCE_DIRECTORY__
+    Trace.trace <| sprintf "configuration:          %s" configuration
+    Trace.trace <| sprintf "debugsymbols:           %s" debugsymbols
+    Trace.trace <| sprintf "targetframeworkversion  %s" targetframeworkversion
+    Trace.trace <| sprintf "optimize:               %s" optimize
+    Trace.trace <| sprintf "solution:               %s" solution
+
 )
 
-// start build
-Target.runOrDefault "Default"
+
+Target.create "Restore" (fun _ ->
+
+    DotNet.restore id |> ignore
+
+)
+
+Target.create "Build" (fun _ ->
+
+    let setMsBuildParams (defaults:MSBuild.CliArguments) =
+        { defaults with
+            Verbosity = Some(Quiet)
+            Targets = ["Build"]
+            Properties =
+                [
+                    "Optimize",         optimize
+                    "DebugSymbols",     debugsymbols
+                    "Configuration",    configuration
+                ]
+         }
+    let setParams (defaults:DotNet.BuildOptions) =
+        { defaults with
+            MSBuildParams = setMsBuildParams defaults.MSBuildParams
+        }
+
+    DotNet.build setParams ("src" @@ "MassTransitServices.sln")
+
+)
+
+open Fake.Core.TargetOperators
+
+"Initialize"
+    ==> "Restore"
+    ==> "Build"
+
+Target.runOrDefault "Build"
