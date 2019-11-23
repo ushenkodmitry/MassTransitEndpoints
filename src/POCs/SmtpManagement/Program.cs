6d5d87509df4dbd6d39c2902d39243154d0c49b6
Ushenko Dmitry <ushenko.dmitry@gmail.com>
2019-11-23 17:32:55 +0300

    Blazor management UI

---
+++ src/POCs/SmtpManagement/Program.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace SmtpManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, builder) => builder.AddEnvironmentVariables())
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
---
+++ src/POCs/SmtpManagement/Startup.cs
@@ -0,0 +1,80 @@
+using System;
+using HostedServices;
+using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Options.MassTransit;
+using HostOptions = Options.RabbitMq.HostOptions;
+
+namespace SmtpManagement
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+
+            services
+                .AddHostedService<MassTransitHostedService>()
+                .AddMassTransit(massTransit =>
+                {
+                    massTransit.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(rabbitMq =>
+                    {
+                        var hostOptions = new HostOptions();
+                        Configuration.GetSection("Host").Bind(hostOptions);
+
+                        var rabbitMqHost = rabbitMq.Host(hostOptions.Host, hostOptions.VirtualHost, host =>
+                        {
+                            host.Username(hostOptions.Username);
+                            host.Password(hostOptions.Password);
+                        });
+
+                        var busOptions = new BusOptions();
+                        Configuration.GetSection("Bus").Bind(busOptions);
+
+                        if (string.Equals("bson", busOptions.Serializer, StringComparison.OrdinalIgnoreCase))
+                            rabbitMq.UseBsonSerializer();
+
+                        rabbitMq.UseExtensionsLogging(provider.GetRequiredService<ILoggerFactory>());
+                    }));
+                });
+
+            services
+                .AddSingleton<ISendEndpointProvider>(provider => provider.GetRequiredService<IBus>());
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
